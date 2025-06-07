ca7528e2bda65c58cefec9e5c7bcc1020b1e2a06
Charles Wilson <charleswilsonk10@gmail.com>
2025-06-07 17:45:59 -0400

    Fix SSR problem

--- CSBS-Dashboard/vite.config.ts
+++ CSBS-Dashboard/vite.config.ts
@@ -4,5 +4,17 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), {
+    name: 'ignore-css-during-ssr',
+    enforce: 'pre',
+    resolveId(id) {
+      if (id.endsWith('.css')) return id;
+    },
+    load(id) {
+      if (id.endsWith('.css')) return '';
+    },
+  },],
+  ssr: {
+    noExternal: ["@mui/x-data-grid"], // Ensure MUI Data Grid is processed in SSR
+  },
 });
